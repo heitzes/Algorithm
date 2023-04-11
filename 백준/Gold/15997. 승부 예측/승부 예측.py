dc5312f3bd472f6f9c0cdefdcf68acbf1e8b751a
Solphi <66466798+heitzes@users.noreply.github.com>
2023-04-11 09:47:18 +0900

    [Gold III] Title: 승부 예측, Time: 68 ms, Memory: 34364 KB -BaekjoonHub

--- 백준/Gold/15997. 승부 예측/승부 예측.py
+++ 백준/Gold/15997. 승부 예측/승부 예측.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
-from itertools import product
 countries = input().split()
 games = [input().split() for _ in range(6)]
+scores = defaultdict(int)
 ans = defaultdict(int)
 
-def same_score(order, ans, p):
+def same_score(order, p):
+    global ans
     if len(order) == 4:
         ans[order[0][1][0]] += 1 * p
         ans[order[1][1][0]] += 1 * p
@@ -37,27 +38,43 @@ def same_score(order, ans, p):
         ans[order[0][1][1]] += 0.5 * p
         ans[order[0][1][2]] += 0.5 * p
         ans[order[0][1][3]] += 0.5 * p
-    return ans
 
-for match in product(range(3), range(3), range(3), range(3), range(3), range(3)):
-    percent = 1
-    wins = defaultdict(int)
-    for ind, v in enumerate(match):
-        percent *= float(games[ind][2+v])
-        if v == 0:
-            wins[games[ind][0]] += 3
-            wins[games[ind][1]] += 0
-        elif v == 1:
-            wins[games[ind][0]] += 1
-            wins[games[ind][1]] += 1
-        else:
-            wins[games[ind][0]] += 0
-            wins[games[ind][1]] += 3
+def find():
+    global scores
     refs = defaultdict(list)
-    if percent != 0.0:
-        for k, v in wins.items():
-            refs[v].append(k)
-        order = sorted(list(refs.items()), reverse=True)
-        ans = same_score(order, ans, percent)
+    for k, v in scores.items():
+        refs[v].append(k)
+    order = sorted(list(refs.items()), reverse=True)
+    return order
+
+def dfs(ind, all):
+    global games, scores, ans
+    if ind == 6:
+        if all != 0.0:
+            order = find()
+            same_score(order, all)
+        return
+
+    # 이김
+    scores[games[ind][0]] += (3 if ind != -1 else 0)
+    scores[games[ind][1]] += (0 if ind != -1 else 0)
+    dfs(ind+1, all * float(games[ind][2] if ind != -1 else 1))
+    scores[games[ind][0]] -= (3 if ind != -1 else 0)
+
+    # 비김
+    scores[games[ind][0]] += (1 if ind != -1 else 0)
+    scores[games[ind][1]] += (1 if ind != -1 else 0)
+    dfs(ind+1, all * float(games[ind][3] if ind != -1 else 1))
+    scores[games[ind][0]] -= (1 if ind != -1 else 0)
+    scores[games[ind][1]] -= (1 if ind != -1 else 0)
+
+    # 짐
+    scores[games[ind][0]] += (0 if ind != -1 else 0)
+    scores[games[ind][1]] += (3 if ind != -1 else 0)
+    dfs(ind+1, all * float(games[ind][4] if ind != -1 else 1))
+    scores[games[ind][1]] -= (3 if ind != -1 else 0)
+
+
+dfs(-1, 1)
 for country in countries:
-    print(ans[country])
+    print(ans[country] / 3) # dfs(-1, 1) 때문에 dfs(0, 1)이 3번 진행됨 
