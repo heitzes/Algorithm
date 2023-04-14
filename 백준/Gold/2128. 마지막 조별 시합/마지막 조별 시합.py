59f62945b640157dd1a4f0c66fdc706805f8d6f2
Solphi <66466798+heitzes@users.noreply.github.com>
2023-04-14 18:30:05 +0900

    [Gold IV] Title: 마지막 조별 시합, Time: 680 ms, Memory: 34120 KB -BaekjoonHub

--- 백준/Gold/2128. 마지막 조별 시합/마지막 조별 시합.py
+++ 백준/Gold/2128. 마지막 조별 시합/마지막 조별 시합.py
@@ -1,47 +1,27 @@
+from collections import defaultdict
 from itertools import combinations
 n, d, k = map(int, input().split())
-student, comb = [[] for _ in range(n)], []
-pbs = [i+1 for i in range(d)]
-for i in range(n):
-    pb = list(map(int, input().split()))
-    for j in range(pb[0]):
-        student[i].append(pb[j+1])
-for k in range(1, k+1):
-    comb.extend(map(list, combinations(pbs, k)))
+pbs = defaultdict(list)
+std = defaultdict(str)
+answer = 0
+combination = list(combinations([i+1 for i in range(d)], k))
 
-def bit_maker(problem):
+def bitmask(it):
+    global d
     bit = 0
-    for p in problem:
-        bit |= (1 << (d-p))
+    for v in it:
+        bit |= (1 << (v-1))
     return bit
-maxi = 0
-for c in comb:
-    c_bit = bit_maker(c)
-    cnt = 0
-    for s in student:
-        s_bit = bit_maker(s)
-        if c_bit | s_bit == c_bit:
-            cnt += 1
-    maxi = max(maxi, cnt)
-print(maxi)
 
-# set 풀이
-# bonus = 0
-# student = defaultdict(set)
-# for i in range(n):
-#     pbs = list(map(int, input().split()))
-#     if pbs[0] == 0:
-#         bonus += 1
-#     for p in range(pbs[0]):
-#         student[i].add(pbs[p+1])
-# comb = []
-# for i in range(1, k+1):
-#     comb.extend(list(map(set, combinations(dlist, i))))
-# maxi = 0
-# for c in comb:
-#     count = 0
-#     for st, pb in student.items():
-#         if len(pb - c) == 0:
-#             count += 1
-#     maxi = max(maxi, count)
-# print(maxi + bonus)
+for i in range(n):
+    student = list(map(int, input().split()))
+    std[i] = bitmask(student[1:])
+
+for comb in combination:
+    problem = bitmask(comb)
+    cnt = 0
+    for student in std.values():
+        if student | problem == problem:
+            cnt += 1
+    answer = max(cnt, answer)
+print(answer)
