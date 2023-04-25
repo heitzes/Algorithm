90b693dc2da27ff307b8ac17782246cfcc3eb6a3
Solphi <66466798+heitzes@users.noreply.github.com>
2023-04-25 13:21:31 +0900

    [unrated] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,16 +1,14 @@
+from string import ascii_lowercase
 def solution(s, skip, index):
     answer = ''
-    skiplist = [i for i in skip]
+    chset = set(ascii_lowercase)
+    chset -= set(skip)
+    chlist = sorted(chset)
+    chlen = len(chlist)
+    
     for ch in s:
-        i = 0
-        while i < index:
-            next_ord = ord(ch) + 1 
-            if next_ord > 122:
-                next_ord -= 26                
-            next_ch = chr(next_ord)
-            if next_ch not in skiplist:
-                i += 1
-            ch = next_ch
-        answer += ch
-        
+        ind = chlist.index(ch)
+        next_ind = ind + index
+        answer += (chlist[next_ind%chlen])
+    
     return answer
