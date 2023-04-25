9be72fdb367965312c6dfbce45286d31ce37be57
Solphi <66466798+heitzes@users.noreply.github.com>
2023-04-25 12:30:39 +0900

    [unrated] Title: 덧칠하기, Time: 3.87 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -1,15 +1,8 @@
-from bisect import bisect_left
-from collections import deque
 def solution(n, m, section):
+    prev = section[0]
     answer = 1
-    start = 0
-    section = deque(section)
-    while True:
-        sec = section[start]
-        ind = bisect_left(section, sec+m)
-        if ind == len(section):
-            break
-        answer += 1
-        start = ind
-
+    for sec in section:
+        if sec >= prev + m:
+            answer += 1
+            prev = sec
     return answer
