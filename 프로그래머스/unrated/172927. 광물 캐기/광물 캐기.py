83aef9b055f5b1e0fcd017dc21116c8a719cec9c
Solphi <66466798+heitzes@users.noreply.github.com>
2023-04-28 15:56:25 +0900

    [unrated] Title: 광물 캐기, Time: 126.67 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.py
@@ -11,16 +11,12 @@ def solution(picks, minerals):
         -> 곡괭이 갯수 (다, 철, 돌) 순열로 구하고 완전탐색
     2. 곡괭이의 배치
         -> (2,1,1)에 대해 다다철돌/다철돌다/.. 이런식으로 사용할 수 있음
-        
         -> 완전탐색으로 최소 피로도 구할거임
     """
     
     answer = 1e9
     needs = math.ceil(len(minerals) / 5)
-    if sum(picks) >= needs:
-        combination = [p for p in product(range(picks[0]+1), range(picks[1]+1), range(picks[2]+1)) if sum(p)==needs]
-    else:
-        combination = [p for p in product(range(picks[0]+1), range(picks[1]+1), range(picks[2]+1)) if sum(p)==sum(picks)]
+    combination = [p for p in product(range(picks[0]+1), range(picks[1]+1), range(picks[2]+1)) if sum(p)==min(sum(picks), needs)]
     pirodo = [[1,1,1], [5,1,1], [25,5,1]]
     translate = {"diamond": 0, "iron": 1, "stone": 2}
 
