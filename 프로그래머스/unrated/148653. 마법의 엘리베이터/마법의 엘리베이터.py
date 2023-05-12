2312e1f06ac9c111ff417d22f2adf9fa20d8b102
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-12 15:27:18 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,13 +1,9 @@
 def solution(storey):
     answer = 0
-    for i in range(1, len(str(storey))):
+    for i in range(1, len(str(storey))+1):
         ref = round(storey/(10**i)) * (10**i)
         answer += abs(ref-storey)/(10**(i-1))
         storey = ref
-    ref = round(storey/(10**len(str(storey)))) * (10**len(str(storey)))
-    if ref != 0:
-        ref -= storey
-        ref = str(ref).replace('0', '')
-        return answer + int(ref) + 1
-    storey = str(storey).replace('0', '')
-    return answer + int(storey)
+    if storey != 0:
+        answer += 1
+    return answer
