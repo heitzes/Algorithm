3fa010af386b5f70454ff1daad852faae372ce13
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-15 14:57:23 +0900

    [level 2] Title: 올바른 괄호, Time: 8.73 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,9 @@
 def solution(s):
-    answer = True
     stack = []
-    # ) 는 넣지 않고, (가 있으면 pop
     for i in s:
-        if i =='(':
+        if not stack or i == '(':
             stack.append(i)
-        elif stack and stack[-1] == '(':
+            continue
+        if stack[-1] == '(':
             stack.pop()
-        else:
-            return False
-    if stack:
-        return False
-    return True
+    return len(stack) == 0
