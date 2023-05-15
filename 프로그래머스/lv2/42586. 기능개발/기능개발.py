8355966a46adb59cf7c887017049544d183ee415
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-15 15:05:00 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,12 +1,14 @@
-import math
 def solution(progresses, speeds):
     answer = [0]
-    baepo = [math.ceil((100-progresses[i])/speeds[i]) for i in range(len(speeds))]
-    now = baepo[0]
-    for b in baepo:
-        if b <= now:
-            answer[-1] += 1
+    timeline = []
+    for p, s in zip(progresses, speeds):
+        timeline.append(-int(-(100-p)//s))
+    cur = timeline[0]
+    while timeline:
+        tpop = timeline.pop(0)
+        if tpop <= cur:
+        	answer[-1] += 1
         else:
-            now = b
             answer.append(1)
+            cur = tpop
     return answer
