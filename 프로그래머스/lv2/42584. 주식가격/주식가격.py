ed5d918249a4cc7edfbe48a37ce57e6cd3c9cef6
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-15 16:28:20 +0900

    [level 2] Title: 주식가격, Time: 21.30 ms, Memory: 15.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,12 +1,8 @@
-from collections import deque
 def solution(prices):
     answer, stack = [0], [[prices.pop(), 0]]
     while prices:
-        ppop = prices.pop()
-        val = 0
-        while stack:
-            if ppop > stack[-1][0]:
-                break
+        ppop, val = prices.pop(), 0
+        while stack and ppop <= stack[-1][0]:
             val += stack[-1][1]
             stack.pop()
         answer.append(val+1)
