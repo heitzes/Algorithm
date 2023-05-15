1bfaea86e41c9fb3f70739e4a50f6782d02a7e41
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-15 23:53:59 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -3,8 +3,8 @@ def solution(numbers):
     changed = []
     for n in numbers:
         ch = (str(n) * (4))[:4]
-        changed.append([ch, 4-len(str(n))])
+        changed.append([ch, len(str(n))])
     changed = sorted(changed, reverse=True)
     for ch, ind in changed:
-        answer += ch[:4-ind]
+        answer += ch[:ind]
     return str(int(answer))
