375f5bcc51fc8524092a4b9cf536ba2c9253ac8b
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-15 21:04:28 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,15 +1,28 @@
 import heapq
 def solution(operations):
-    nums = []
+    minh, maxh = [], []
+    popped = set()
+    cnt = 0
     for op in operations:
         mode, n = op.split(" ")
         if mode == "I":
-            heapq.heappush(nums, int(n))
-        elif nums:
-            if int(n) == 1:
-                nums.pop()
-            else:
-                heapq.heappop(nums)
-    if not nums:
+            heapq.heappush(minh, int(n))
+            heapq.heappush(maxh, -int(n))
+            cnt += 1
+            continue
+        if cnt == 0:
+            continue
+        if int(n) == 1:
+            while -maxh[0] in popped:
+                heapq.heappop(maxh)
+            popped.add(-heapq.heappop(maxh))
+        else:
+            while minh[0] in popped:
+                heapq.heappop(minh)
+            popped.add(heapq.heappop(minh))
+        cnt -= 1
+    maxh = [-i for i in maxh]
+    cross = set(minh) & set(maxh)
+    if not cross:
         return [0, 0]
-    return [max(nums), nums[0]]
+    return [max(cross), min(cross)]
