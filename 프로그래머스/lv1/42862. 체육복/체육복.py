e00614ea8909388222b3bdfacfcf9353a6be9f8a
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-16 15:37:47 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -0,0 +1,12 @@
+def solution(n, lost, reserve):
+    answer = 0
+    reserve, lost = set(set(reserve)-set(lost)), set(set(lost)-set(reserve))
+    cant = len(lost)
+    for l in sorted(lost):
+        if l-1 in reserve:
+            reserve -= set([l-1])
+            cant -= 1
+        elif l+1 in reserve:
+            reserve -= set([l+1])
+            cant -= 1
+    return n - cant
