ea8bbb79ec30de3247e682fa3c40fb04f3662052
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-16 14:13:23 +0900

    [level 1] Title: 모의고사, Time: 2.67 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -4,15 +4,14 @@ def solution(answers):
     supo_1 = ([1,2,3,4,5] * ((n // 5)+1))[:n] 
     supo_2 = ([2,1,2,3,2,4,2,5] * ((n // 8)+1))[:n]
     supo_3 = ([3,3,1,1,2,2,4,4,5,5,] * ((n // 10)+1))[:n]
-    s1, s2, s3 = 0, 0, 0
+    scores = [0,0,0]
     for i in range(n):
         if supo_1[i] == answers[i]:
-            s1 += 1
+            scores[0] += 1
         if supo_2[i] == answers[i]:
-            s2 += 1
+            scores[1] += 1
         if supo_3[i] == answers[i]:
-            s3 += 1
-    scores = [s1, s2, s3]
+            scores[2] += 1
     maxi = max(scores)
     for i in range(1, 4):
         if scores[i-1] == maxi:
