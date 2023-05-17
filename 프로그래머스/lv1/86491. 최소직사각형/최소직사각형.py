2d40e0183de7e13e46cb1f882f478ef0f87f1290
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-17 12:55:51 +0900

    [level 1] Title: 최소직사각형, Time: 9.71 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,7 +1,11 @@
 def solution(sizes):
     answer = 0
-    new_sizes = [sorted(size) for size in sizes]
-    max_w = max([i[0] for i in new_sizes])
-    max_h = max([i[1] for i in new_sizes])    
-    
-    return max_w * max_h
+    current = sizes.pop()
+    for size in sizes:
+        comp1 = [max(size[0], current[0]), max(size[1], current[1])]
+        comp2 = [max(size[1], current[0]), max(size[0], current[1])]
+        if comp1[0]*comp1[1] > comp2[0]*comp2[1]:
+            current = comp2
+        else:
+            current = comp1
+    return current[0] * current[1]
