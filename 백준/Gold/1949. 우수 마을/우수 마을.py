6988a2067942befb9ef72526a9a111031a0a5dd8
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-17 17:08:14 +0900

    [Gold II] Title: 우수 마을, Time: 488 ms, Memory: 37668 KB -BaekjoonHub

--- 백준/Gold/1949. 우수 마을/우수 마을.py
+++ 백준/Gold/1949. 우수 마을/우수 마을.py
@@ -5,25 +5,16 @@ n = int(input())
 people = list(map(int, input().split()))
 cost = {i: [0, people[i]] for i in range(n)}
 graph = defaultdict(list)
+vi = set([0])
 for i in range(n-1):
     a, b = map(int, input().split())
     graph[a-1].append(b-1)
     graph[b-1].append(a-1)
-def make_tree(i):
-    tree = defaultdict(list)
-    vi, dq = set([i]), deque([i])
-    while dq:
-        dpop = dq.popleft()
-        for ch in graph[dpop]:
-            if ch in vi:
-                continue
-            tree[dpop].append(ch)
-            dq.append(ch)
-            vi.add(ch)
-    return tree
-tree = make_tree(0)
 def postOrder(node):
-    for ch in tree[node]:
+    for ch in graph[node]:
+        if ch in vi:
+            continue
+        vi.add(ch)
         postOrder(ch)
         cost[node][0] += max(cost[ch])
         cost[node][1] += cost[ch][0]
