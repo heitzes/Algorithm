eea46fd579d29f82fdae164ce0bb41d5211e025c
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-17 18:48:29 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,17 +1,19 @@
 def solution(n, costs):
-    answer, cnt = 0, n-1
-    costs = sorted(costs, key = lambda x: (x[-1], x[0], x[1]))
-    nset = set()
-    while cnt != 0:
-        for cost in costs:
-            if not nset:
-                break
-            if cost[0] in nset and cost[1] in nset:
-                continue
-            if cost[0] in nset or cost[1] in nset:
-                break
-        nset.update(cost[:-1])
-        answer += cost[-1]
-        costs.remove(cost)
-        cnt -= 1
-    return answer
+    answer = 0
+    uf = [-1] * n
+    costs = sorted(costs, key=lambda x: x[-1])
+    def find(node):
+        if uf[node] < 0: 
+            return node
+        uf[node] = find(uf[node])
+        return uf[node]
+    def union(a, b):
+        ra, rb = find(a), find(b)
+        if ra == rb:
+            return False
+        uf[rb] = ra
+        return True
+    for cost in costs:
+        if union(cost[0], cost[1]):
+            answer += cost[-1]
+    return answer 
