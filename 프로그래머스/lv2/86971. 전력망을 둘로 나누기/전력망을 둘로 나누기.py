c42bdaa8ab4fe587e609b7b559b74fe20118a20d
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-17 14:04:59 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.13 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,23 +1,21 @@
 from collections import defaultdict
+def make_tree(vi, graph, tree, nodes, n):
+    vi.add(n)
+    for ch in graph[n]:
+        if ch in vi:
+            continue
+        tree[n].append(ch)
+        make_tree(vi, graph, tree, nodes, ch)
+        nodes[n] += nodes[ch]
+    nodes[n] += 1
+    return tree, nodes
 def solution(n, wires):
     answer = float('inf')
     graph = defaultdict(list)
     for wire in wires:
         graph[wire[0]].append(wire[1])
         graph[wire[1]].append(wire[0])
-    vi = set()
-    tree = defaultdict(list)
-    nodes = defaultdict(int)
-    def make_tree(n):
-        vi.add(n)
-        for ch in graph[n]:
-            if ch in vi:
-                continue
-            tree[n].append(ch)
-            make_tree(ch)
-            nodes[n] += nodes[ch]
-        nodes[n] += 1
-    make_tree(1)
+    tree, nodes = make_tree(set(), graph, defaultdict(list), defaultdict(int), 1)
     available = set(nodes.values())
     for av in available:
         cut = n-av
