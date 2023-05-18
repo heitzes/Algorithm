d2a5ef5a30920d0b816bbb8566a0d2b5f4978bba
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-18 16:46:43 +0900

    [Gold IV] Title: 트리, Time: 360 ms, Memory: 52688 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.py
+++ 백준/Gold/4803. 트리/트리.py
@@ -1,16 +1,19 @@
 import sys
-from collections import defaultdict, deque
-# input = sys.stdin.readline
-# 방문한 정점을 알아낸다
-def bfs(node):
-    vi, dq = set([node]), deque([node])
-    while dq:
-        dpop = dq.popleft()
-        for ch in graph[dpop]:
-            if ch not in vi:
-                vi.add(ch)
-                dq.append(ch)
-    return vi
+input = sys.stdin.readline
+def find(k):
+    if uf[k] < 0:
+        return k
+    uf[k] = find(uf[k])
+    return uf[k]
+def union(a, b):
+    ra, rb = find(a), find(b)
+    if ra == rb:
+        cycle.add(ra)
+        return 
+    if rb > ra:
+        uf[ra] = rb
+    else:
+        uf[rb] = ra
 commands, order = [], 1
 while True:
     a, b = map(int, input().split())
@@ -20,18 +23,14 @@ while True:
 while commands:
     v, e = commands.pop(0)
     cnt = 0
-    graph = defaultdict(list)
+    uf = [-1] * (v+1)
+    cycle = set()
     for i, j in commands[:e]:
-        graph[i].append(j)
-        graph[j].append(i)
-    visited = set([]) # 중복을 피하기위해 사용
+        union(i, j)
+    cycle = set([find(i) for i in cycle])
     for k in range(1, v+1):
-        if k not in visited:
-            vi = bfs(k) # k에서 시작하여 방문한 node들
-            edges = sum([len(graph[i]) for i in vi]) // 2 # 간선의 수
-            if len(vi)-1 == edges: # 정점수보다 간선수가 하나 적으면 트리임
-                cnt += 1
-            visited.update(vi) # 트리에 속한 node들은 재방문할 필요 없으니 업데이트
+        if uf[k]==-1 and k not in cycle:
+            cnt += 1
     commands = commands[e:]
     if cnt == 0:
         print("Case {}: No trees.".format(order))
