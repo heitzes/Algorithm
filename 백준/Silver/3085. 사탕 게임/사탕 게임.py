91ada54a67a6b148bde3deb99194fc7851773557
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-19 18:56:43 +0900

    [Silver II] Title: 사탕 게임, Time: 640 ms, Memory: 118152 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,18 +1,13 @@
 n = int(input())
 candy1 = [list(input()) for _ in range(n)]
 def count(row):
-    maxi, idx = 0, 0
-    while idx != len(row)-1:
-        cnt, color = 1, row[idx]
-        for i in range(idx+1, len(row)):
-            if row[i] == color:
-                cnt += 1
-                continue
-            idx = i
-            maxi = max(maxi, cnt)
-            break
+    maxi, cnt = 1, 1
+    for idx in range(len(row)-1):
+        if row[idx] == row[idx+1]:
+            cnt += 1
         else:
-            return max(maxi, cnt)
+            cnt = 1
+        maxi = max(cnt, maxi) # 코드를 단순하게 만들기 위해 조금 비효율적으로 짜도 괜찮다.
     return maxi
 maximum = 0
 for i in range(n):
