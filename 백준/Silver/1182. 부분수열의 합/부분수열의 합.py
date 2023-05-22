b37dee454f0d51cba3e383d69a0e33631c1b6839
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-22 23:12:29 +0900

    [Silver II] Title: 부분수열의 합, Time: 384 ms, Memory: 117860 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,14 +1,13 @@
-from itertools import product
 n, s = map(int, input().split())
 nlist = list(map(int, input().split()))
-pd = list(product(range(2), repeat=n))
 answer = 0
-pd.pop(0)
-for p in pd:
-    summ = 0
-    for i in range(n):
-        if p[i] == 1:
-            summ += nlist[i]
-    if summ == s:
-        answer += 1
+def recursive(i, summ):
+    global answer
+    if i == n:
+        if len(summ) > 0 and sum(summ) == s:
+            answer += 1
+        return
+    recursive(i+1, summ)
+    recursive(i+1, summ + [nlist[i]])
+recursive(0, [])
 print(answer)
