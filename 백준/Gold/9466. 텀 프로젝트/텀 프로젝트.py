242a492aff368c0ca4f8b1de5d3b40f51d78aa17
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-27 15:05:09 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2600 ms, Memory: 59416 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -3,32 +3,26 @@ sys.setrecursionlimit(10**6)
 from collections import defaultdict
 t = int(input())
 
-def case(students):
-    slen = len(students)
-    cycle = set()
-    graph = dict()
-    for i, s in enumerate(students, 1):
-        if i == s: 
-            cycle.add(s)
-        graph[i] = s
-    return cycle, graph
-
-def dfs(nodes, now):
-    vi.add(now)
-    if graph[now] not in vi:
-        vset = dfs(nodes, graph[now])
-        if now in vset:
-            cycle.update(nodes)
-            cycle.add(graph[now])
-    nodes.add(graph[now])
-    return nodes
+def dfs(node):
+    global cnt
+    vi[node] = 1
+    nxt = nlist[node]
+    if vi[nxt]:
+        if not fi[nxt]:
+            start = node
+            while nxt != start:
+                nxt = nlist[nxt]
+                cnt += 1
+            cnt += 1
+    else: dfs(nlist[node])
+    fi[node] = 1
 
 for _ in range(t):
     n = int(input())
-    nlist = list(map(int, input().split()))
-    vi = set()
-    cycle, graph = case(nlist)
+    nlist = [0] + list(map(int, input().split()))
+    vi = [0] * (n+1)
+    fi = [0] * (n+1)
+    cnt = 0
     for i in range(1, n+1):
-        if i not in vi:
-            dfs(set(), i)
-    print(n - len(cycle))
+        if not vi[i]: dfs(i)
+    print(n - cnt)
