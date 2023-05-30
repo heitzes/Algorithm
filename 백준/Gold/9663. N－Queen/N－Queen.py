e7c5aa144dd2e5862dc32dc2c4fc17090cbc92f2
doongsae <66466798+heitzes@users.noreply.github.com>
2023-05-30 20:56:27 +0900

    [Gold IV] Title: N-Queen, Time: 19712 ms, Memory: 151412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,9 +1,10 @@
 n = int(input())
 answer = 0
-def backtracking(ind, nums):
+def backtracking(nums):
     global answer
+    last_idx = len(nums)-1
     for i, v in enumerate(nums[:-1]):
-        if abs(i-(ind-1)) == abs(v-nums[-1]):
+        if abs(i-last_idx) == abs(v-nums[-1]):
             return
     if len(nums) == n:
         answer += 1
@@ -11,7 +12,7 @@ def backtracking(ind, nums):
         if i in nums: continue
         if nums and abs(nums[-1]-i) == 1: continue
         nums.append(i)
-        backtracking(ind+1, nums)
+        backtracking(nums)
         nums.pop()
     return answer
-print(backtracking(0, []))
+print(backtracking([]))
