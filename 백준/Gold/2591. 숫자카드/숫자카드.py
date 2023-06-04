b7984de3adf7cb5605f8814ca954ab02b325f570
doongsae <66466798+heitzes@users.noreply.github.com>
2023-06-04 16:24:33 +0900

    [Gold V] Title: 숫자카드, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/2591. 숫자카드/숫자카드.py
+++ 백준/Gold/2591. 숫자카드/숫자카드.py
@@ -1,29 +1,9 @@
-n = input()
-nlist = ['0']
+nlist = input()
 dp = [0] * (41)
-dp[1], dp[2] = 1, 2
-for i in range(3, 41):
-    dp[i] = dp[i-1] + dp[i-2] 
-for i in range(len(n)):
-    if '1' <= nlist[-1][-1]+n[i] <= '34':
-        if nlist[-1][-1]+n[i] in ['10', '20', '30']:
-            npop = nlist.pop()
-            if len(npop[:-1]) > 0:
-                nlist.append(npop[:-1])
-            nlist.append(npop[-1]+n[i])
-        else:
-            nlist[-1] += n[i]
-    else:
-        nlist.append(n[i])
-nlist = nlist[1:]
-ans = 1
-for num in nlist:
-    if int(num) == 0:
-        print(0)
-        break
-    if num[-1] == '0':
-        ans *= dp[len(str(int(num)))-1]
-    else:
-        ans *= dp[len(str(int(num)))]
-else:
-    print(ans)
+dp[0], dp[1] = 1, 1 if 1 <= int(nlist[0]) <= 34 else 0
+for i in range(1, len(nlist)):
+    if '1' <= (nlist[i-1]+nlist[i]) <= '34':
+        dp[i+1] += dp[i-1]
+    if nlist[i] != '0':
+        dp[i+1] += dp[i]
+print(dp[len(nlist)])
