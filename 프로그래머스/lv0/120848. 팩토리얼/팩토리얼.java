edfaffac48d0e755fd43ae68c70470ce3708fa24
doongsae <66466798+heitzes@users.noreply.github.com>
2023-06-15 16:58:23 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n) {
         int answer = 1;
         int factorial = 1;
-        while (factorial * answer < n) {
+        while (factorial * (answer+1) <= n) {
             answer += 1;
             factorial *= answer;
         }
