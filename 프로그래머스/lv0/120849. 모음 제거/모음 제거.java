5e94fef3e98e9683b2b3082dc43dcc9e8cb1fc51
doongsae <66466798+heitzes@users.noreply.github.com>
2023-06-15 17:11:01 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        answer = my_string.replaceAll("[aeiou]", "");
+        return answer;
+    }
+}
