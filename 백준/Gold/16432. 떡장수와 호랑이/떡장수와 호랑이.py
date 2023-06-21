99e077ccb5de2b79947200db659d4f1370790a97
doongsae <66466798+heitzes@users.noreply.github.com>
2023-06-21 16:40:31 +0900

    [Gold IV] Title: 떡장수와 호랑이, Time: 76 ms, Memory: 35488 KB -BaekjoonHub

--- 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.py
+++ 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.py
@@ -4,31 +4,21 @@ def input():
     return sys.stdin.readline().rstrip()
 n = int(input())
 nlist = [list(map(int, input().split()))[1:] for _ in range(n)]
-dp = [[-1]*10 for _ in range(n)]
+dp = [[-1]*10 for _ in range(n+1)]
 
 anslist = []
-def dfs(ind, dduck):
+def dfs(ind, dduck, ans):
     global n
-    if ind == 0 and len(ans)==n:
-        anslist.append(tuple(ans))
-        return True
+    if ind == 0:
+        for s in ans[::-1]:
+            print(s)
+        exit(0)
     if dp[ind][dduck] != -1:
         return dp[ind][dduck]
     dp[ind][dduck] = False
-    for d in range(1, 10):
-        if d in nlist[ind-1] and d != dduck:
-            ans.append(d)
-            dp[ind][dduck] |= dfs(ind-1, d)
-            ans.pop()
+    for d in nlist[ind-1]:
+        if d != dduck:
+            dp[ind][dduck] |= dfs(ind-1, d, ans + [d])
     return dp[ind][dduck]
-
-for d in nlist[-1]:
-    if dp[n-1][d] == -1:
-        ans = [d]
-        dfs(n-1, d)
-        
-if not anslist:
-    print(-1)
-else:
-    for s in anslist[0][::-1]:
-        print(s)
+dfs(n, 0, [])
+print(-1)
