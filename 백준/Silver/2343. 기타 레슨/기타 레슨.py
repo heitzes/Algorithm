4da3873f1233795dc4325a58d3d8003e4fab52f3
doongsae <66466798+heitzes@users.noreply.github.com>
2023-06-27 15:31:07 +0900

    [Silver I] Title: 기타 레슨, Time: 152 ms, Memory: 126264 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.py
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -5,7 +5,7 @@ dp = [0] * n
 dp[0] = nlist[0]
 for i in range(1, n):
     dp[i] = dp[i-1] + nlist[i]
-left, right = 1, sum(nlist)
+left, right = 0, sum(nlist)
 
 def check(k):
     global m
@@ -20,11 +20,10 @@ def check(k):
     if cnt > m: return False
     return True 
 
-while left <= right:
+while left + 1 < right:
     mid = (left + right) // 2
     if check(mid):
-        right = mid - 1
-        answer = mid
+        right = mid
     else:
-        left = mid + 1
-print(answer)
+        left = mid
+print(right)
