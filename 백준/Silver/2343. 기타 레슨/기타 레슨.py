31f9df5100e457f379982db97f68e7799fef0677
doongsae <66466798+heitzes@users.noreply.github.com>
2023-06-27 15:13:34 +0900

    [Silver I] Title: 기타 레슨, Time: 152 ms, Memory: 126264 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -0,0 +1,30 @@
+from bisect import bisect_right
+n, m = map(int, input().split())
+nlist = list(map(int, input().split()))
+dp = [0] * n
+dp[0] = nlist[0]
+for i in range(1, n):
+    dp[i] = dp[i-1] + nlist[i]
+left, right = 1, sum(nlist)
+
+def check(k):
+    global m
+    ref, cnt = 0, 1
+    if max(nlist) > k: return False
+    for lecture in nlist:
+        if ref + lecture <= k:
+            ref += lecture
+        else:
+            cnt += 1
+            ref = lecture
+    if cnt > m: return False
+    return True 
+
+while left <= right:
+    mid = (left + right) // 2
+    if check(mid):
+        right = mid - 1
+        answer = mid
+    else:
+        left = mid + 1
+print(answer)
