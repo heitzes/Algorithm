824e9cf84f06fd57b6ae5a1bdf67e2915f8f9201
doongsae <66466798+heitzes@users.noreply.github.com>
2023-07-04 20:46:08 +0900

    [Gold V] Title: 문자열 판별, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Gold/16500. 문자열 판별/문자열 판별.py
+++ 백준/Gold/16500. 문자열 판별/문자열 판별.py
@@ -1,12 +1,15 @@
 string = input()
 n = int(input())
-wset = set([input() for _ in range(n)])
-dp = [0] * (len(string)+1)
-dp[0] = 1
-for i in range(len(string)):
-    if dp[i] != 1:
-        continue
-    for word in wset:
-        if string[i:i+len(word)] == word:
-            dp[i+len(word)] = 1
-print(dp[-1])
+wlist = [input() for _ in range(n)]
+dp = [-1] * (len(string)+1)
+def dfs(idx):
+    global n
+    if dp[idx] != -1:
+        return dp[idx]
+    dp[idx] = 1
+    for word in wlist:
+        if string[idx:idx+len(word)] == word:
+            dfs(idx + len(word))
+    return dp[idx]
+dfs(0)
+print(dp[-1] if dp[-1]==1 else 0)
