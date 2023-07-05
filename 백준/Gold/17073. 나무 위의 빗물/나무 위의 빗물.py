9f4f02a79b4a9337906b6a38c8e21cc5a08f930c
doongsae <66466798+heitzes@users.noreply.github.com>
2023-07-05 16:18:50 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 3532 ms, Memory: 265128 KB -BaekjoonHub

--- 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
@@ -5,39 +5,20 @@ def input():
 n, w = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 tree = [[] for _ in range(n+1)]
-water = [0] * (n+1)
-water[1] = w
 for _ in range(n-1):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 vi = set()
-def make_tree(idx, h):
-    global height
+def make_tree(idx):
     vi.add(idx)
     for ch in graph[idx]:
         if ch in vi:
             tree[ch].append(idx)
             continue
-        make_tree(ch, h+1)
-make_tree(1, 0)
-
-def spread(idx):
-    length = len(tree[idx])
-    if not length: return
-    for ch in tree[idx]:
-        water[ch] = water[idx] / length
-    water[idx] = 0
-
-def travel(idx):
-    spread(idx)
-    for ch in tree[idx]:
-        travel(ch)
-
-travel(1)
-cnt, ans = 0, 0
+        make_tree(ch)
+make_tree(1)
+leaf = 0
 for i in range(1, n+1):
-    if water[i] > 0: 
-        cnt += 1
-        ans += water[i]
-print(ans/cnt)
+    if not len(tree[i]): leaf += 1
+print(w/leaf)
