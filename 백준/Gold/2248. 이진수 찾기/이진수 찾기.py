3c9a059639258cb4a11649e06557e70bc284ff34
doongsae <66466798+heitzes@users.noreply.github.com>
2023-07-07 18:54:13 +0900

    [Gold III] Title: 이진수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2248. 이진수 찾기/이진수 찾기.py
+++ 백준/Gold/2248. 이진수 찾기/이진수 찾기.py
@@ -3,30 +3,25 @@ sys.setrecursionlimit(10**6)
 n, l, i = map(int, input().split())
 dp = [[-1]*(l+1) for _ in range(n+1)]
 def call_dp(idx, cnt):
-    if idx == n:
+    if idx == n or cnt == 0:
         dp[idx][cnt] = 1
-        return 1
-    if cnt == 0:
         return 1
     if dp[idx][cnt] != -1:
         return dp[idx][cnt]
-    dp[idx][cnt] = call_dp(idx+1, cnt) + call_dp(idx+1, cnt-1)
+    dp[idx][cnt] = call_dp(idx+1, cnt)
+    if cnt > 0: dp[idx][cnt] += call_dp(idx+1, cnt-1)
     return dp[idx][cnt]
 call_dp(0, l)
-answer = ''
 def dfs(idx, cnt, nums, order):
-    global answer
-    if idx < n and cnt == 0:
+    if idx == n: 
+        return nums
+    if cnt == 0:
         nums.extend(['0']*(n-idx))
-        answer = ''.join(nums)
-        return
-    if idx == n:
-        answer = ''.join(nums)
-        return
+        return nums
     left = dp[idx+1][cnt]
     if i > order + left:
-        dfs(idx+1, cnt-1, nums + ['1'], order + left)
+        return dfs(idx+1, cnt-1, nums + ['1'], order + left)
     else:
-        dfs(idx+1, cnt, nums + ['0'], order)
-dfs(0, l, [], 0)
-print(answer)
+        return dfs(idx+1, cnt, nums + ['0'], order)
+answer = dfs(0, l, [], 0)
+print(''.join(answer))
