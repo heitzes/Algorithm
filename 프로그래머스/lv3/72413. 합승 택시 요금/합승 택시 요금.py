c9a8b6a1a1f5b84eeaeec1efaa1847ac4b8814e0
doongsae <66466798+heitzes@users.noreply.github.com>
2023-07-13 11:08:59 +0900

    [level 3] Title: 합승 택시 요금, Time: 19.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,33 +1,23 @@
 import heapq
 def solution(n, s, a, b, fares):
-    answer = 0
-    
+    answer = 1e9
     graph = [[] for _ in range(n+1)]
     cost = [[0]*(n+1) for _ in range(n+1)]
     for i, j, k in fares:
         graph[i].append(j)
         graph[j].append(i)
-        cost[i][j] = k
-        cost[j][i] = k
-
+        cost[i][j] = cost[j][i] = k
     def daijkstra(start):
-        pq = []
-        dist = [-1] * (n + 1)
-        for i in range(1, n+1):
-            if i == start: 
-                heapq.heappush(pq, [0, i])
-                dist[i] = 0
-            else: 
-                dist[i] = 1e9
-
+        pq = [[0, start]]
+        dist = [1e9] * (n + 1)
+        dist[start] = 0
         while pq:
             cur, node = heapq.heappop(pq)
             for ch in graph[node]:
                 if dist[ch] > cur + cost[node][ch]:
                     dist[ch] = cur + cost[node][ch]
                     heapq.heappush(pq, [dist[ch], ch])
-        return dist
-    answer = 1e9
+        return dist   
     for i in range(1, n+1):
         dist = daijkstra(i)
         answer = min(answer, dist[s] + dist[a] + dist[b])
