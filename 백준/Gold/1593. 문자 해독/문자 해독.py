ea8da74505c387e6b2ca323cb7eb329f489efc37
doongsae <66466798+heitzes@users.noreply.github.com>
2023-08-07 14:54:15 +0900

    [Gold V] Title: 문자 해독, Time: 4548 ms, Memory: 39652 KB -BaekjoonHub

--- 백준/Gold/1593. 문자 해독/문자 해독.py
+++ 백준/Gold/1593. 문자 해독/문자 해독.py
@@ -7,9 +7,6 @@ answer = 0 if wcnt!=scnt else 1
 for i in range(slen-wlen):
     scnt[string[i]] -= 1
     scnt[string[i+wlen]] += 1
-    for w in word:
-        if wcnt[w] != scnt[w]:
-            break
-    else:
+    if wcnt == scnt:
         answer += 1
 print(answer)
