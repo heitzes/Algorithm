dd53dab51526a460a545a25dd2379658e508fbb1
doongsae <66466798+heitzes@users.noreply.github.com>
2023-08-26 15:56:19 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 600 ms, Memory: 150116 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
@@ -1,47 +1,40 @@
 from collections import deque
 n, m = map(int, input().split())
 nlist = [list(map(int, list(input()))) for _ in range(n)]
-vi = [[0]*m for _ in range(n)]
-dp = [[0]*m for _ in range(n)]
 ans = [[0]*m for _ in range(n)]
-p = [[[0,0] for _ in range(m)] for _ in range(n)]
 dx, dy = [0, 0, 1, -1], [1, -1 , 0, 0]
-def bfs(a, b):
-    global n, m
-    dq = deque([[a, b]])
-    check = set([(a, b)])
-    p[a][b] = [a, b]
-    vi[a][b] = 1
-    cnt = 1
+gnum = 1 # 그룹번호
+gvi = [[0]*m for _ in range(n)] # 방문여부 겸 그룹번호 기록
+gcnt = dict() # 그룹에 속한 node 수 기록
+def bfs(i, j):
+    global n, m, gnum
+    dq = deque([[i, j]])
+    gvi[i][j] = gnum
+    gcnt[gnum] = gcnt.get(gnum, 0) + 1
     while dq:
         x, y = dq.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if not (0<=nx<n and 0<=ny<m): continue
-            if vi[nx][ny] != 1 and nlist[nx][ny] == 0:
+            if not nlist[nx][ny] and not gvi[nx][ny]:
                 dq.append([nx, ny])
-                check.add((nx, ny))
-                vi[nx][ny] = 1
-                p[nx][ny] = [a, b]
-                cnt += 1
-    for x, y in check:
-        dp[x][y] = cnt
+                gvi[nx][ny] = gnum
+                gcnt[gnum] += 1
+    gnum += 1
 for i in range(n):
     for j in range(m):
-        if not nlist[i][j] and not vi[i][j]:
+        if not nlist[i][j] and not gvi[i][j]:
             bfs(i, j)
 for x in range(n):
     for y in range(m):
         if not nlist[x][y]: continue
-        ps = set()
+        gcheck = set()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if not (0<=nx<n and 0<=ny<m): continue
-            if nlist[nx][ny] == 0:
-                ps.add(tuple(p[nx][ny]))
-        for i, j in ps:
-            ans[x][y] += dp[i][j]
-        ans[x][y] += 1
-        ans[x][y] %= 10
+            if gvi[nx][ny] and gvi[nx][ny] not in gcheck:
+                ans[x][y] += gcnt[gvi[nx][ny]]
+                gcheck.add(gvi[nx][ny])
+        ans[x][y] = (ans[x][y] + 1) % 10
 for row in ans:
     print(''.join([str(i) for i in row]))
