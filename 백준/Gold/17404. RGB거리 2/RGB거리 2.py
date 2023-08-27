c00834ad2f18ccab84415bd0c0845af3207a0fec
doongsae <66466798+heitzes@users.noreply.github.com>
2023-08-27 19:02:14 +0900

    [Gold IV] Title: RGB거리 2, Time: 148 ms, Memory: 115192 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.py
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -1,19 +1,14 @@
 n = int(input())
 nlist = [list(map(int, input().split())) for _ in range(n)]
 answer = 1e9
-combination = [[0,1], [0,2], [1,0], [1,2], [2,0], [2,1]]
-def make_dp(comb):
-    global n, answer
+for i in range(3):
     dp = [[1e9, 1e9, 1e9] for _ in range(n)]
-    dp[0][comb[0]] = nlist[0][comb[0]]
-    for i in range(1, n-1):
-        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + nlist[i][0]
-        dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + nlist[i][1]
-        dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + nlist[i][2]
-    for i in range(3):
-        if i != comb[1]:
-            dp[n-1][comb[1]] = min(dp[n-1][comb[1]], dp[n-2][i] + nlist[n-1][comb[1]])
-    answer = min(answer, dp[n-1][comb[1]])
-for comb in combination:
-    make_dp(comb)
+    dp[0][i] = nlist[0][i]
+    for k in range(1, n):
+        dp[k][0] = min(dp[k-1][1], dp[k-1][2]) + nlist[k][0]
+        dp[k][1] = min(dp[k-1][0], dp[k-1][2]) + nlist[k][1]
+        dp[k][2] = min(dp[k-1][0], dp[k-1][1]) + nlist[k][2]
+    for k in range(3):
+        if i != k:
+            answer = min(dp[-1][k], answer)
 print(answer)
