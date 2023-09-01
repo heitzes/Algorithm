06c59449aae546d8f51b3f078094c33b4d087e68
doongsae <66466798+heitzes@users.noreply.github.com>
2023-09-01 19:51:57 +0900

    [Gold V] Title: 암호코드, Time: 128 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.py
+++ 백준/Gold/2011. 암호코드/암호코드.py
@@ -1,9 +1,10 @@
-n = list(map(int, list(input())))
-dp = [[0, 0] for _ in range(len(n))]
-dp[0][0] = 1 if n[0] != 0 else 0
-for i in range(1, len(n)):
-    if n[i] != 0:
-        dp[i][0] = sum(dp[i-1])
-    if n[i-1] == 1 or n[i-1] == 2 and n[i] <= 6:
-        dp[i][1] = dp[i-1][0]
-print(sum(dp[-1]) % 1000000)
+string = input()
+alpha = set([str(i) for i in range(1, 27)])
+dp = [0] * (len(string)+1)
+dp[0] = 1 ### 초기값 설정 !!!!
+for i in range(1, len(string)+1):
+    for j in range(1, 3):
+        if i - j < 0 : continue
+        if string[i-j:i] in alpha:
+            dp[i] += dp[i-j]
+print(dp[-1] % 1000000)
