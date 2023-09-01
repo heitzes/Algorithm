95df201ffaae219703db901dc7664bfd7b18e455
doongsae <66466798+heitzes@users.noreply.github.com>
2023-09-01 13:31:02 +0900

    [Gold II] Title: 사전, Time: 124 ms, Memory: 115136 KB -BaekjoonHub

--- 백준/Gold/1256. 사전/사전.py
+++ 백준/Gold/1256. 사전/사전.py
@@ -1,30 +1,22 @@
 n, m, k = map(int, input().split())
-length = n + m
-dp = [[0] * (i+1) for i in range(201)]
-for i in range(201):
-    for j in range(i+1):
-        if i == j == 0:
-            continue
-        elif i == 0 or j == 0 or i == j:
-            dp[i][j] = 1
-        else:
-            dp[i][j] = dp[i-1][j-1] + dp[i-1][min(j, i-1-j)]
-def solution():
-    global n, m, k
-    if dp[length][m] < k:
-        return -1
-    pos, pset = 0, set()
-    while m > 0:
-        if k == 1: break
-        for i in range(m, length-pos+1):
-            if dp[i][m] >= k and i - 1 >= m:
-                k -= dp[i-1][m]
-                m -= 1
-                pos = length - (i)
-                pset.add(pos)
-                break
-    answer = ['z' if i in pset else 'a' for i in range(length)]
-    if not pset: pset.add(-1)
-    answer[max(pset)+1:] = ['a'] * (length-(max(pset)+1)-(m)) + ['z'] * m
-    return ''.join(answer)
-print(solution())
+dp = [[0] * (m+1) for _ in range(n+1)]
+answer = ''
+for i in range(n+1):
+    for j in range(m+1):
+        if i == j == 0: continue
+        if i == 0 or j == 0: dp[i][j] = 1
+        else: dp[i][j] = dp[i-1][j] + dp[i][j-1]
+def find(x, y, k):
+    global answer
+    if dp[x][y] < k: return -1
+    if k == 1: 
+        answer += 'a'*(x) + 'z'*(y)
+        return answer
+    if dp[x-1][y] >= k:
+        answer += 'a'
+        find(x-1, y, k)
+    else:
+        answer += 'z'
+        find(x, y-1, k-dp[x-1][y])
+    return answer
+print(find(n, m, k))
