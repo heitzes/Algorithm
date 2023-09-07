4b287cbdfa335dc5a1b9aa4467d076f70d451b18
doongsae <66466798+heitzes@users.noreply.github.com>
2023-09-07 17:42:08 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 7.71 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -9,14 +9,15 @@ def solution(alp, cop, problems):
         maxa = max(maxa, p[0])
         maxc = max(maxc, p[1])
     arr = [[1e9] * (maxc+1) for _ in range(maxa+1)]
-    heap = [[0, -alp, -cop]] # 시간, -알고, -코딩
+    heap = [[0, alp, cop]] # 시간, -알고, -코딩
     while heap:
         time, algo, code = heapq.heappop(heap)
+        if algo >= maxa and code >= maxc: return time
         for p in problems:
             needa, needc = p[0], p[1]
-            if needa > -algo or needc > -code: continue
-            na, nc = -algo+p[2], -code+p[3]
+            if needa > algo or needc > code: continue
+            na, nc = algo+p[2], code+p[3]
             if arr[min(na, maxa)][min(nc, maxc)] > time + p[-1]:
                 arr[min(na, maxa)][min(nc, maxc)] = time + p[-1]
-                heapq.heappush(heap,[time + p[-1], -min(na, maxa), -min(nc, maxc)])
+                heapq.heappush(heap, [time + p[-1], min(na, maxa), min(nc, maxc)])
     return arr[-1][-1]
