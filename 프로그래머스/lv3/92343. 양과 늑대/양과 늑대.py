d00055e3dca67c2a2349759454ba7f7bbb1362f3
doongsae <66466798+heitzes@users.noreply.github.com>
2023-09-07 21:34:19 +0900

    [level 3] Title: 양과 늑대, Time: 0.43 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
@@ -2,13 +2,14 @@ def solution(info, edges):
     answer = 0
     graph = [set() for _ in range(17)]
     for p, ch in edges: graph[p].add(ch)
+    cnt = 0
     def dfs(node, sheep, wolf, nxt):
-        nonlocal answer
+        nonlocal answer,cnt
         if wolf >= sheep: return
         answer = max(answer, sheep)
-        nxt = (nxt - set([node])) | graph[node]
+        nxt |= graph[node]
         for ch in nxt:
-            if not info[ch]: dfs(ch, sheep+1, wolf, nxt)
-            else: dfs(ch, sheep, wolf+1, nxt)
-    dfs(0, 1, 0, graph[0])
+            if not info[ch]: dfs(ch, sheep+1, wolf, (nxt - set([ch])))
+            else: dfs(ch, sheep, wolf+1, (nxt - set([ch])))
+    dfs(0, 1, 0, set())
     return answer
