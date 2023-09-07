bb5f3e03cabc4ec60d25a5ae48d8620feea40aa2
doongsae <66466798+heitzes@users.noreply.github.com>
2023-09-07 16:49:02 +0900

    [unrated] Title: 표 병합, Time: 223.99 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150366. 표 병합/표 병합.py
@@ -0,0 +1,57 @@
+def solution(commands):
+    answer = []
+    r, c = 50, 50
+    board = [['']*(c+1) for _ in range(r+1)]
+    p = [['{0}*{1}'.format(i, j) for j in range(c+1)] for i in range(r+1)]
+    def find(x, y):
+        if p[x][y] == '{0}*{1}'.format(x,y): return p[x][y]
+        rx, ry = p[x][y].split("*")
+        p[x][y] = find(int(rx), int(ry))
+        return p[x][y]
+    def union(x1, y1, x2, y2):
+        rx1, ry1 = map(int, find(x1, y1).split("*"))
+        rx2, ry2 = map(int, find(x2, y2).split("*"))
+        if rx1==rx2 and ry1==ry2: return
+        if board[rx1][ry1]:
+            p[rx2][ry2] = '{0}*{1}'.format(rx1, ry1)
+        else:
+            p[rx1][ry1] = '{0}*{1}'.format(rx2, ry2)
+    def update(x, y, k):
+        rx, ry = map(int, find(x, y).split("*"))
+        board[rx][ry] = k
+    def updateV(k, v):
+        for x in range(1, r+1):
+            for y in range(1, c+1):
+                rx, ry = map(int, find(x, y).split("*"))
+                if board[rx][ry] == k:
+                    update(rx, ry, v)
+    def dunion(x, y):
+        root = find(x, y)
+        rx, ry = map(int, root.split("*"))
+        string = board[rx][ry]
+        reset = []
+        for i in range(1, r+1):
+            for j in range(1, c+1):
+                if find(i, j) == root:
+                    reset.append([i, j])
+        for i, j in reset:
+            p[i][j] = '{0}*{1}'.format(i, j)
+            board[i][j] = ''
+        board[x][y] = string
+    def printV(x, y):
+        rx, ry =  map(int, find(x, y).split("*"))
+        if board[rx][ry] !='': return board[rx][ry]
+        else: return "EMPTY"
+    for cmds in commands:
+        clist = cmds.split()
+        if clist[0] == 'UPDATE':
+            if len(clist) == 4:
+                update(int(clist[1]), int(clist[2]), clist[3])
+            else: updateV(clist[1], clist[2])
+        elif clist[0] == "MERGE":
+            union(int(clist[1]), int(clist[2]), int(clist[3]), int(clist[4]))
+        elif clist[0] == "UNMERGE":
+            dunion(int(clist[1]), int(clist[2]))
+        elif clist[0] == "PRINT":
+            answer.append(printV(int(clist[1]),int(clist[2])))
+    return answer
